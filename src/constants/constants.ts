d5c63d7690c2830063bd50130098990c57d20d6c
Noritinho <marnori2001@gmail.com>
2024-05-13 02:36:05 -0300

    feat: fetch on pokeapi

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,1 +1,1 @@
-export const pokeapiUrl = 'https://pokeapi.co/api/v2';
+export const pokeapiUrl = 'https://pokeapi.co/api/v2/pokemon/';
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,19 @@
 import axios from 'axios';
+import { IPokemon } from '../interface/pokemonInterface';
+import { pokeapiUrl } from '../constants/constants';
 
-axios('https://pokeapi.co/api/v2/pokemon/ditto').then((response) => {
-  console.log(response.data);
-});
+export class FetchPokemon {
+  public async getPokemonByNumbers(ids: number[]): Promise<IPokemon[]> {
+    const promises = ids.map((id) => axios(`${pokeapiUrl}${id}`));
+    const responses = await Promise.all(promises);
+    return responses.map((response) => response.data.id);
+  }
+}
+
+async function executeFetch() {
+  const fetchPokemon = new FetchPokemon();
+  const pokemons = await fetchPokemon.getPokemonByNumbers(Array.from({ length: 151 }, (_, i) => i + 1));
+  pokemons.forEach((pokemon) => console.log(pokemon));
+}
+
+console.log(executeFetch());
